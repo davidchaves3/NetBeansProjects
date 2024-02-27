8075985e990a2817acfbf92a21e723b9f50489c8
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2024-02-27 19:11:09 -0300

    teste

---
+++ aula01/src/main/java/com/mycompany/aula01/Aula01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula01;
+
+/**
+ *
+ * @author Chaves
+ */
+public class Aula01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
