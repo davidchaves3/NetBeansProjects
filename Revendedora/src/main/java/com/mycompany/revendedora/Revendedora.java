12a91ff39580eacc7073d18381493a0e6ddbfb84
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2024-04-25 19:17:34 -0300

    POO

---
+++ Revendedora/src/main/java/com/mycompany/revendedora/Revendedora.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.revendedora;
+
+public class Revendedora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
