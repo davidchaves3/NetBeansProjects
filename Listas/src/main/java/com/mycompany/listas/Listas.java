4eb88dc41e1a4f533e0f388008d2bf29c958f9a7
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2024-06-05 11:19:23 -0300

    update

---
+++ Listas/src/main/java/com/mycompany/listas/Listas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listas;
+
+/**
+ *
+ * @author Chaves
+ */
+public class Listas {
+
+    public static void main(String[] args) {
+        String nome = "David";;
+        System.out.println(nome.charAt(0));
+    }
+}
